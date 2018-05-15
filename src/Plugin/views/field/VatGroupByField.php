96e58e9dd0dd16e626752a1e84dd7100ed56a07e
Oliver Steiner <oliver@mollo.ch>
2018-05-15 17:49:44 +0200

    replace goenner_typ  with member_type

--- src/Plugin/views/field/VatGroupByField.php
+++ src/Plugin/views/field/VatGroupByField.php
@@ -1,25 +1,26 @@
 <?php
-  /**
-   * @file
-   * Definition of Drupal\views_admintools\Plugin\views\field\VatGroupByField
-   */
+/**
+ * @file
+ * Definition of Drupal\views_admintools\Plugin\views\field\VatGroupByField
+ */
 
-  namespace Drupal\views_admintools\Plugin\views\field;
+namespace Drupal\views_admintools\Plugin\views\field;
 
-  use Drupal\Core\Form\FormStateInterface;
-  use Drupal\taxonomy\Entity\Term;
-  use Drupal\views\Plugin\views\field\FieldPluginBase;
-  use Drupal\views\ResultRow;
-  use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Drupal\taxonomy\Entity\Vocabulary;
 
-  /**
-   * Field handler to add Edit and Delete Buttons.
-   *
-   * @ingroup views_field_handlers
-   *
-   * @ViewsField("vat_group_by_field")
-   */
-  class VatGroupByField extends FieldPluginBase {
+/**
+ * Field handler to add Edit and Delete Buttons.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("vat_group_by_field")
+ */
+class VatGroupByField extends FieldPluginBase
+{
 
 
     var $html_tags = ['div', 'span', 'h1', 'h2', 'h3', 'h4'];
@@ -28,8 +29,9 @@
     /**
      * @{inheritdoc}
      */
-    public function query() {
-      // Leave empty to avoid a query on this field.
+    public function query()
+    {
+        // Leave empty to avoid a query on this field.
     }
 
     /**
@@ -37,163 +39,169 @@
      *
      * @return array
      */
-    protected function defineOptions() {
-      $options = parent::defineOptions();
-      $options['group_by_field_name'] = ['default' => FALSE];
-      $options['group_by_vocabulary'] = ['default' => '-'];
-      $options['group_by_html_tag'] = ['default' => 0];
-      $options['group_by_class'] = ['default' => ''];
+    protected function defineOptions()
+    {
+        $options = parent::defineOptions();
+        $options['group_by_field_name'] = ['default' => FALSE];
+        $options['group_by_vocabulary'] = ['default' => '-'];
+        $options['group_by_html_tag'] = ['default' => 0];
+        $options['group_by_class'] = ['default' => ''];
 
 
-      return $options;
+        return $options;
     }
 
     /**
      * Provide the options form.
      */
-    public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    public function buildOptionsForm(&$form, FormStateInterface $form_state)
+    {
 
 
-      $form['text'] = [
-        '#markup' => $this->t('Fieldname to group'),
-      ];
+        $form['text'] = [
+            '#markup' => $this->t('Fieldname to group'),
+        ];
 
-      $form['group_by_field_name'] = [
-        '#title' => $this->t('Field Name'),
-        '#type' => 'textfield',
-        '#default_value' => $this->options['group_by_field_name'],
-      ];
-
-
-      $form['group_by_html_tag'] = [
-        '#title' => $this->t('HTML Tag'),
-        '#type' => 'select',
-        '#default_value' => $this->options['group_by_html_tag'],
-        '#options' => $this->html_tags,
-      ];
-
-      $form['group_by_class'] = [
-        '#title' => $this->t('Custom CSS Class'),
-        '#type' => 'textfield',
-        '#default_value' => $this->options['group_by_class'],
-      ];
-
-      // or voc
-      $vocabularies = Vocabulary::loadMultiple();
-
-      $options_voc = [];
-      $options_voc[] = '-';
-
-      foreach ($vocabularies as $key => $type) {
-        $options_voc[$key] = $type->label();
-      }
-      $form['group_by_vocabulary'] = [
-        '#title' => $this->t('Taxonomy Field'),
-        '#type' => 'select',
-        '#default_value' => $this->options['group_by_vocabulary'],
-        '#options' => $options_voc,
-      ];
+        $form['group_by_field_name'] = [
+            '#title' => $this->t('Field Name'),
+            '#type' => 'textfield',
+            '#default_value' => $this->options['group_by_field_name'],
+        ];
 
 
-      parent::buildOptionsForm($form, $form_state);
+        $form['group_by_html_tag'] = [
+            '#title' => $this->t('HTML Tag'),
+            '#type' => 'select',
+            '#default_value' => $this->options['group_by_html_tag'],
+            '#options' => $this->html_tags,
+        ];
+
+        $form['group_by_class'] = [
+            '#title' => $this->t('Custom CSS Class'),
+            '#type' => 'textfield',
+            '#default_value' => $this->options['group_by_class'],
+        ];
+
+        // or voc
+        $vocabularies = Vocabulary::loadMultiple();
+
+        $options_voc = [];
+        $options_voc[] = '-';
+
+        foreach ($vocabularies as $key => $type) {
+            $options_voc[$key] = $type->label();
+        }
+        $form['group_by_vocabulary'] = [
+            '#title' => $this->t('Taxonomy Field'),
+            '#type' => 'select',
+            '#default_value' => $this->options['group_by_vocabulary'],
+            '#options' => $options_voc,
+        ];
+
+
+        parent::buildOptionsForm($form, $form_state);
     }
 
     /**
      * @{inheritdoc}
      */
-    public function render(ResultRow $values) {
+    public function render(ResultRow $values)
+    {
 
-      // Options
-      $options['class'] = $this->options['group_by_class'];
-      $options['tag'] = $this->options['group_by_html_tag'];
-      $options['field'] = $this->options['group_by_field_name'];
-      $options['vocabulary'] = $this->options['group_by_vocabulary'];
+        // Options
+        $options['class'] = $this->options['group_by_class'];
+        $options['tag'] = $this->options['group_by_html_tag'];
+        $options['field'] = $this->options['group_by_field_name'];
+        $options['vocabulary'] = $this->options['group_by_vocabulary'];
 
-      // CSS Class Array
-      $class = ['vat-group', $options['class']];
-      $text = '';
+        // CSS Class Array
+        $class = ['vat-group', $options['class']];
+        $text = '';
 
-      if (($options['field'] != FALSE) OR ($options['vocabulary'] != '-')) {
+        if (($options['field'] != FALSE) OR ($options['vocabulary'] != '-')) {
 
-        // Load Entitys
-        $entity = $values->_entity;
+            // Load Entitys
+            $entity = $values->_entity;
 
-        if ($options['vocabulary'] != '-') {
-          $group_by_field_name = $options['vocabulary'];
+            if ($options['vocabulary'] != '-') {
+                $group_by_field_name = $options['vocabulary'];
+            } else {
+                $group_by_field_name = $options['field'];
+            }
+
+
+            // ist ein solches Feld vorhanden?
+
+            $fieldname = 'field_' . $group_by_field_name;
+
+
+            // Get Taxonomy ID
+            if ($entity->hasField($fieldname)) {
+
+                // get Field Content
+                $field_content = $entity->get($fieldname)->getValue();
+
+
+                // Get Taxonomy ID
+                if ($field_content && $field_content[0] && $field_content[0]['target_id']) {
+                    $tid_new = $field_content[0]['target_id'];
+                } else {
+                    $tid_new = false;
+                }
+
+
+                // check if last tid is same as new tid
+                $tid_old = \Drupal::config('views_admintools.settings')
+                    ->get('group_by');
+
+
+                if ($tid_old != $tid_new) {
+
+                    // Store New tid globaly:
+                    $config = \Drupal::service('config.factory')
+                        ->getEditable('views_admintools.settings');
+                    $config->set('group_by', $tid_new)->save();
+
+                    // Get Taxonomy Name from tid
+                    if ($tid_new && Term::load($tid_new)) {
+                        $term_name = Term::load($tid_new)->get('name')->value;
+                    } else {
+                        $term_name = '';
+                    }
+
+                    // Output
+                    $text = $term_name;
+                    $class[] = 'vat-group-new';
+                } else {
+
+                    // Output
+                    $text = '';
+                    $class[] = 'vat-group-old';
+
+                }
+
+            }
+
         }
-        else {
-          $group_by_field_name = $options['field'];
-        }
+
+        $tag = $this->html_tags[$options['tag']];
 
 
-        // ist ein solches Feld vorhanden?
+        $elements['vat_group_by'] = [
+            '#type' => 'html_tag',
+            '#tag' => $tag,
+            '#attributes' => [
+                'class' => $class,
+            ],
+            '#value' => $text,
 
-        $fieldname = 'field_' . $group_by_field_name;
-
-
-        // Get Taxonomy ID
-        if ($entity->hasField($fieldname)) {
-
-          // get Field Content
-          $field_content = $entity->get($fieldname)->getValue();
-
+        ];
 
 
-          // Get Taxonomy ID
-          $tid_new = $field_content[0]['target_id'];
+        $elements['#attached']['library'][] = 'views_admintools/views_admintools.group_by';
 
-
-          // check if last tid is same as new tid
-          $tid_old = \Drupal::config('views_admintools.settings')
-            ->get('group_by');
-
-
-          if ($tid_old != $tid_new) {
-
-            // Store New tid globaly:
-            $config = \Drupal::service('config.factory')
-              ->getEditable('views_admintools.settings');
-            $config->set('group_by', $tid_new)->save();
-
-            // Get Taxonomy Name from tid
-            $term_name = Term::load($tid_new)
-              ->get('name')->value;
-
-            // Output
-            $text = $term_name;
-            $class[] = 'vat-group-new';
-          }
-
-          else {
-
-            // Output
-            $text = '';
-            $class[] = 'vat-group-old';
-
-          }
-
-        }
-
-      }
-
-      $tag = $this->html_tags[$options['tag']];
-
-
-      $elements['vat_group_by'] = [
-        '#type' => 'html_tag',
-        '#tag' => $tag,
-        '#attributes' => [
-          'class' => $class,
-        ],
-        '#value' => $text,
-
-      ];
-
-
-      $elements['#attached']['library'][] = 'views_admintools/views_admintools.group_by';
-
-      return $elements;
+        return $elements;
 
     }
 
-  }
+}
