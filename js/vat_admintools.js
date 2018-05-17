9b5b6e511966ad0788e2ab46fe143c0690d81141
Oliver Steiner <oliver@mollo.ch>
2018-05-17 22:20:03 +0200

    ..

--- js/vat_admintools.js
+++ js/vat_admintools.js
@@ -7,11 +7,7 @@
     attach: function (context, settings) {
 
         $('#drupal-modal').on('show.bs.modal', function (e) {
-            if (!data) return e.preventDefault() // stops modal from being shown
-            alert('test');
         })
-
-
     }
   };
 
